f2f53f37816acd62dde072b7e687908eec8c184d
xawlw <abdulazizawlw@gmail.com>
2021-10-16 11:42:16 +0200

    vendor: Change MD5 to SHA256
    * Now we generate SHA256 instead of MD5
    
    Change-Id: I464fd2540005599681e8a896d95f35721f61145e
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -29,7 +29,7 @@ bacon: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) $(SHA256) $(CUSTOM_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(CUSTOM_TARGET_PACKAGE).sha256sum
 	echo -e ${CL_BLD}${CL_RED}"===============================-Package complete-==============================="${CL_RED}
 	echo -e ${CL_BLD}${CL_GRN}"Zip: "${CL_RED} $(CUSTOM_TARGET_PACKAGE)${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"MD5: "${CL_RED}" `cat $(CUSTOM_TARGET_PACKAGE).md5sum | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"SHA256: "${CL_RED}" `cat $(CUSTOM_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
 	echo -e ${CL_BLD}${CL_GRN}"Size:"${CL_RED}" `du -sh $(CUSTOM_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
 	echo -e ${CL_BLD}${CL_GRN}"TimeStamp:"${CL_RED}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
 	echo -e ${CL_BLD}${CL_GRN}"Integer Value:"${CL_RED}" `wc -c $(CUSTOM_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
