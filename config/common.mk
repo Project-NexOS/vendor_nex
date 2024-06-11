0eb5ed79eecaf61dd7bb6156ad3c9537299902c2
Phạm Tiên Sinh <song.long.dai.duong@gmail.com>
2024-06-12 00:57:04 +0800

    vendor: Build GameSpace

--- config/common.mk
+++ config/common.mk
@@ -164,6 +164,10 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/%/libfuse-lite.so \
     system/%/libntfs-3g.so
 
+# Game space
+PRODUCT_PACKAGES += \
+    GameSpace
+
 # Openssh
 PRODUCT_PACKAGES += \
     scp \
